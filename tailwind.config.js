29464e1628c6b879246512ebbb34d654f4acfad0
jjonyo <jonghyuk96@cau.ac.kr>
2022-03-30 03:01:01 +0900

    FEAT : (#2) Add Login Modal UI and  Naver Login API

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      black: '#fff',
+      white: '#fff',
+      gray: {
+        100: '#f7fafc',
+        900: '#1a202c',
+      },
+    },
+  },
+};
