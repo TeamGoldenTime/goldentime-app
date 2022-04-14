95b278561e18dfcb3edf3182f56b9cc5e4644c2a
jjonyo <jonghyuk96@cau.ac.kr>
2022-04-14 12:29:44 +0900

    FEAT : (#11) Lost Report Step3

--- src/screens/lostReport/LostReportLocation.tsx
+++ src/screens/lostReport/LostReportLocation.tsx
@@ -1,24 +1,138 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import tw from 'tailwind-rn';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import MapView, { Marker } from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
+import { StackNavigationProp } from '@react-navigation/stack';
 
-const LostReportLocation = ({ navigation }) => {
+import ReportLayout from './components/ReportLayout';
+import { stepper3 } from './components/stepper3';
+import ReportInput from './components/ReportInput';
+import marker from '../../../assets/image/marker.png';
+
+interface LostReportLocationProps {
+  navigation: StackNavigationProp<any>;
+}
+
+interface LocationStateProps {
+  latitude: number;
+  longitude: number;
+}
+
+const LostReportLocation: React.FC<LostReportLocationProps> = ({
+  navigation,
+}) => {
+  const [location, setLocation] = useState<LocationStateProps | null>(null);
+  const [area, setArea] = useState('');
+
+  const requestPermission = async () => {
+    return Geolocation.requestAuthorization('whenInUse');
+  };
+
+  const run = async () => {
+    const result = await requestPermission();
+
+    if (result === 'granted') {
+      Geolocation.getCurrentPosition(
+        ({ coords }) => {
+          setLocation({
+            latitude: coords.latitude,
+            longitude: coords.longitude,
+          });
+        },
+        error => {
+          console.log(error.code, error.message);
+        },
+        { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
+      );
+    }
+  };
+
+  useEffect(() => {
+    run();
+  }, []);
+
+  const onClickBackButton = () => {
+    navigation.goBack();
+  };
+
+  const onClickFinishButton = () => {
+    //TODO : 폼 작성 완료 후 동작
+
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'main' }],
+    });
+  };
+
+  const onChangeArea = (text: string) => {
+    setArea(text);
+  };
+
   return (
-    <View style={tw('mt-20')}>
-      <Text>step3</Text>
-      <TouchableOpacity
-        onPress={() =>
-          navigation.reset({
-            index: 0,
-            routes: [{ name: 'main' }],
-          })
-        }>
-        <Text>next</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.goBack()}>
-        <Text>back</Text>
-      </TouchableOpacity>
-    </View>
+    <ReportLayout
+      type="step3"
+      navigation={navigation}
+      title="분실신고"
+      mainDescription={'반려동물의 분실위치를\n지정해주세요.'}
+      subDescription=""
+      stepper={stepper3}
+      onClickBackButton={onClickBackButton}
+      onClickFinishButton={onClickFinishButton}>
+      <View
+        style={[
+          {
+            height: hp('54%'),
+          },
+          tw('w-full p-1 pr-3'),
+        ]}>
+        {location && (
+          <>
+            <MapView
+              style={tw('flex-1')}
+              initialRegion={{
+                latitude: location?.latitude,
+                longitude: location?.longitude,
+                latitudeDelta: 0.003,
+                longitudeDelta: 0.003,
+              }}
+              onRegionChange={region => {
+                setLocation({
+                  latitude: region.latitude,
+                  longitude: region.longitude,
+                });
+              }}
+              onRegionChangeComplete={region => {
+                setLocation({
+                  latitude: region.latitude,
+                  longitude: region.longitude,
+                });
+              }}>
+              <Marker
+                coordinate={{
+                  latitude: location.latitude,
+                  longitude: location.longitude,
+                }}
+                image={marker}
+              />
+            </MapView>
+            <View
+              style={[
+                { opacity: 0.5 },
+                tw(
+                  'absolute self-center w-64 h-8 bg-black rounded-xl mt-3 items-center justify-center',
+                ),
+              ]}>
+              <Text style={tw('text-white text-base text-center')}>
+                지도를 움직여 위치를 지정하세요
+              </Text>
+            </View>
+          </>
+        )}
+        <ReportInput title="상세지역" text={area} onChangeText={onChangeArea} />
+      </View>
+    </ReportLayout>
   );
 };
 
